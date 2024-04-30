f3db20e1bab34d24c5d0bb35d90eb91ecb9b1ca4
Nathan HERVIER <113121626+Elli610@users.noreply.github.com>
2024-04-30 02:04:42 +0200

    Update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,7 +7,6 @@ const config: HardhatUserConfig = {
     // Add a local development network
     scroll_testnet: {
       url: "https://scroll-testnet.rpc.grove.city/v1/a7a7c8e2", // Your local Ethereum node URL
-      accounts: ["c806e5e10ba22628e91e9730865be9bc92f8fd14ca5f455d1a0e4f6937efa4bc"]
     },
     // Add more networks here if needed
   },
